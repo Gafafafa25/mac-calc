0125a93e246f8473a4f7280dc7b9556a71453d79
name <gafarova.tasia@gmail.com>
2025-04-29 13:42:15 +0300

    bug fixing

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let screenValue = 0
 const digits  = document.getElementsByClassName("digit")
 for (const btn of digits) {
-    btn.addEventListener("click", () => {
-        console.log(this)
+    btn.addEventListener("click", (e) => {
+        console.log(e.target.textContent)
     })
 }
 console.log(digits)
