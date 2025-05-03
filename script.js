7c44bb9570f06d04bf99fb6b8930a711f63e774b
name <gafarova.tasia@gmail.com>
2025-05-03 15:51:50 +0300

    AC button digits handlers

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let screenValue = 0
+let screenValue = "0"
 const digits  = document.getElementsByClassName("digit")
 for (const btn of digits) {
     btn.addEventListener("click", (e) => {
-        console.log(e.target.textContent)
+        const d = e.target.textContent
+        if (document.getElementById("screen").innerText === "0") {
+            screenValue = d
+        }
+        else {
+            screenValue += d
+        }
+        update()
     })
 }
-console.log(digits)
+
+document.getElementById("clearButton").addEventListener("click", (e) => {
+    screenValue = "0"
+    update()
+})
+
+const update = () => {
+    document.getElementById("screen").innerText = screenValue
+}
